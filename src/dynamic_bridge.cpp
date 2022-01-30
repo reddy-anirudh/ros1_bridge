1ceb5b9400bdfe025781a08b6f5fbdffbcf86b5d
Anirudh Reddy <anirudh_kondapally-reddy@jp.honda>
2022-01-30 18:07:28 +0900

    Correct te filter

--- src/dynamic_bridge.cpp
+++ src/dynamic_bridge.cpp
@@ -146,6 +146,11 @@ void update_bridge(
   for (auto ros1_publisher : ros1_publishers) {
     // identify topics available as ROS 1 publishers as well as ROS 2 subscribers
     auto topic_name = ros1_publisher.first;
+    std::string honda_interface = "honda_interface";
+    std::size_t found = topic_name.find(honda_interface);
+    if(found == std::string::npos){
+      continue;
+    }
     std::string ros1_type_name = ros1_publisher.second;
     std::string ros2_type_name;
 
@@ -215,11 +220,6 @@ void update_bridge(
   for (auto ros2_publisher : ros2_publishers) {
     // identify topics available as ROS 1 subscribers as well as ROS 2 publishers
     auto topic_name = ros2_publisher.first;
-    std::string honda_interface = "honda_interface";
-    std::size_t found = topic_name.find(honda_interface);
-    if(found == std::string::npos){
-      continue;
-    }
     std::string ros2_type_name = ros2_publisher.second;
     std::string ros1_type_name;
 
